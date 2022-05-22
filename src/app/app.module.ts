99ff1db2620fe0f9b3ba210aa85291d3134af153
Stephaniedenis <93406932+Stephaniedenis@users.noreply.github.com>
2022-05-22 15:45:23 -0400

    ajout des components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { AddReviewComponent } from './components/add-review/add-review.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    MovieComponent,
+    AddReviewComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
